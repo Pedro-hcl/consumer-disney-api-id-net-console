641bcfe7390c210ee59548ba9e0fccc817c8db18
Pedro Leão <121962652+Pedro-hcl@users.noreply.github.com>
2026-09-09 21:35:18 -0300

    Add files via upload

---
+++ ConsumerDisneyIdApi/Models/ConsumerDisneyIdApi.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumerDisneyIdApi.Models
+{
+
+
+    public class Data
+    {
+    
+    public class Character
+    {
+        public string? name { get; set; }
+
+        public string? imageUrl { get; set; }
+    }
+}
+}
---
+++ ConsumerDisneyIdApi/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+using ConsumerDisneyIdApi.Models;
+
+WriteLine($"Digite o Id do personagem que deseja consultar:");
+
+    var id = ReadLine();
+
+    var enderecoUrl = $"https://api.disneyapi.dev/character/{id}";
+
+WriteLine($"Consultando o endereço na url: {enderecoUrl}");
+
+var client = new HttpClient();
+
+try
+{
+    
+HttpResponseMessage response = await client.GetAsync(enderecoUrl);
+response.EnsureSuccessStatusCode();
+
+string respostaApi= await response.Content.ReadAsStringAsync();
+
+Data? idapi = System.Text.Json.JsonSerializer.Deserialize<Data>(respostaApi);
+
+WriteLine($"\nNome do personagem: {idapi?.name}");
+WriteLine($"Url da imagem:  {idapi?.imageUrl}");
+
+    
+}   
+
+catch (Exception ex)
+{
+    
+    WriteLine($"Ocorreu um erro ao consultar o endereço: {ex.Message}");
+}
+
